615ef9a190f720b89f445920710bcc0cb5aaf3f6
Philip (flip) Kromer <flip@infochimps.org>
2011-01-22 21:22:41 -0600

    Made directory names for cookbooks follow opscode standards

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Infochimps Org"
+maintainer_email "coders@infochimps.org"
+license          "Apache 2.0"
+description      "Installs/Configures pig"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
+version          "0.1"
+
+depends           "hadoop_cluster"
