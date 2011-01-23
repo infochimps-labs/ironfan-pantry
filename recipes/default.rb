0de1d6f111d76f13d82712c8dd2bcc1e801c2f09
Philip (flip) Kromer <flip@infochimps.org>
2011-01-22 21:22:41 -0600

    Made directory names for cookbooks follow opscode standards

---
+++ recipes/default.rb
@@ -0,0 +1,63 @@
+#
+# Author:: Benjamin Black (<b@b3k.us>)
+# Cookbook Name:: cassandra
+# Recipe:: default
+#
+# Copyright 2010, Benjamin Black
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "java"
+include_recipe "thrift"
+include_recipe "runit"
+
+
+%w[ sun-java6-jdk  sun-java6-bin ].each{|pkg| package(pkg) }
+
+%w[ cassandra avro
+].each{|pkg| gem_package(pkg){ action :install } }
+
+#%w[ simplejson avro
+#].each{|pkg| easy_install_package(pkg){ action :install } }
+
+user "cassandra" do
+  uid       '330'
+  gid       "nogroup"
+  shell     "/bin/false"
+  action    :create
+  not_if{ node[:etc][:passwd] && node[:etc][:passwd]['cassandra'] }
+end
+
+# ignore this please
+directory('/ebs1/data'){ owner("root"); group("admin"); mode("0775"); action(:create) }
+
+[ "/var/lib/cassandra", "/var/log/cassandra",
+  node[:cassandra][:data_file_dirs],
+  node[:cassandra][:commitlog_dir],
+].flatten.each do |cassandra_dir|
+  directory cassandra_dir do
+    owner    "cassandra"
+    group    "root"
+    mode     "0755"
+    action   :create
+    recursive true
+  end
+end
+
+directory "/etc/cassandra" do
+  owner     "root"
+  group     "root"
+  mode      "0755"
+  action    :create
+end
---
+++ recipes/install_from_git.rb
@@ -0,0 +1,57 @@
+cassandra_home        = node[:cassandra][:cassandra_home]
+cassandra_install_dir = cassandra_home + '-git'
+
+directory File.dirname(cassandra_home) do
+  mode         '0755'
+  owner        'root'
+  group        'admin'
+  action       :create
+  recursive true
+end
+
+git cassandra_install_dir do
+  repository    node[:cassandra][:git_repo]
+  action        :sync
+  group         'admin'
+  revision      node[:cassandra][:git_revision]
+end
+
+bash 'install from source' do
+  user         'root'
+  cwd          cassandra_install_dir
+  code <<EOF
+  ant build jar gen-thrift-py
+  mv                conf/cassandra.yaml conf/cassandra.yaml.orig
+  ln -nfs #{cassandra_install_dir}/conf/* /etc/cassandra/
+  chmod a+x bin/*
+  true
+EOF
+  only_if{ Dir["#{cassandra_install_dir}/build/apache-cassandra-*.jar"].blank? }
+end
+
+link cassandra_home do
+  to            cassandra_install_dir
+  action        :create
+end
+
+%w[cassandra schematool cassandra-cli clustertool nodetool sstablekeys].each do |util|
+  link "/usr/local/bin/#{util}" do
+    to          "#{cassandra_home}/bin/#{util}"
+    action      :create
+  end
+end
+
+# check out other version
+# patches
+# sudo apt-get install -y asciidoc source-highlight libboost-regex-dev libboost-dev libboost-system-dev libboost-dev
+# svn export --force . build/avro-src-$VERSION || rsync -alvi ./ ./build/avro-src-$VERSION --exclude={build,.git,.svn,dist,*.cache,lang/c++/config,lang/py/avro.egg-info,lang/ruby/{pkg,Manifest,avro.gemspec}}
+
+bash 'Push compatible Jackson into hadoop if hadoop exists' do
+  code <<EOF
+mv /usr/lib/hadoop/lib/jackson-*1.0.1.jar /tmp && true ;
+cp /usr/local/share/cassandra/lib/jackson-* /usr/lib/hadoop/lib/
+true
+EOF
+  only_if{ File.exists?('/usr/lib/hadoop/lib') }
+  only_if{ Dir['/usr/lib/hadoop/lib/jackson*1.[456789]*'].blank? }
+end
