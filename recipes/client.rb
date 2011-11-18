71123f51a6a364192bd446752cf3883fbd80fdb2
Philip (flip) Kromer <flip@infochimps.org>
2011-11-18 01:39:51 -0600

    fixes for cookbooks from big attribute rename

--- recipes/client.rb
+++ recipes/client.rb
@@ -22,4 +22,4 @@
 include_recipe "zookeeper::default"
 
 # Stuff the Zookeeper jars into the classpath
-node[:hadoop][:extra_classpaths][:zookeeper] = "#{node[:zookeeper][:home_dir]/zookeeper.jar" if node[:hadoop] and node[:hadoop][:extra_classpaths]
+node[:hadoop][:extra_classpaths][:zookeeper] = "#{node[:zookeeper][:home_dir]}/zookeeper.jar" if node[:hadoop] and node[:hadoop][:extra_classpaths]
