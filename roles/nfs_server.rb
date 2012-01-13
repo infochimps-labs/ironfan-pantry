299e5972831d1372a241068ffffaf0d33def4eb7
Travis Dempsey <dempsey.travis@gmail.com>
2012-01-13 15:10:30 -0600

    updated nfs role/recipe wih new defaults and new announce pattern

--- roles/nfs_server.rb
+++ roles/nfs_server.rb
@@ -5,9 +5,3 @@ run_list *%w[
   nfs
   nfs::server
 ]
-
-default_attributes({
-    :nfs => { :exports => {
-        '/home' => { :nfs_options => '*.internal(rw,no_root_squash,no_subtree_check)' },
-      } },
-  })
