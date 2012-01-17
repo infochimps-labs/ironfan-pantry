09a326b45b6e6d601d1c1f4a00a896d1a89d2236
Dhruv Bansal <dhruv@ph.utexas.edu>
2012-01-17 12:53:54 -0600

    add w3m-el for dhruv

--- vendor/infochimps/package_set/attributes/default.rb
+++ vendor/infochimps/package_set/attributes/default.rb
@@ -50,7 +50,7 @@ default[:package_set][:install]          = %w[ base dev sysadmin ]
 default[:package_set][:pkgs][:base]      = %w[ tree git zip openssl wget curl runit libyaml-dev libxslt1-dev ]
 default[:package_set][:gems][:base]      = %w[ bundler rake ]
 
-default[:package_set][:pkgs][:dev]       = %w[ emacs23-nox elinks colordiff ack exuberant-ctags ]
+default[:package_set][:pkgs][:dev]       = %w[ emacs23-nox elinks w3m-el colordiff ack exuberant-ctags ]
 default[:package_set][:gems][:dev]       = %w[
   activesupport activemodel extlib json yajl-ruby awesome_print addressable cheat
   yard jeweler rspec  watchr pry configliere gorillib highline formatador choice rest-client wirble hirb
