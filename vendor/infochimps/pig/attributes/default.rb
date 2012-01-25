05ebca6c18066213263efc31eb1b628a7d6cfaf8
Philip (flip) Kromer <flip@infochimps.org>
2012-01-24 19:26:08 -0600

    version 0.9.1 not available on some mirrors, moving to 0.9.1

--- vendor/infochimps/pig/attributes/default.rb
+++ vendor/infochimps/pig/attributes/default.rb
@@ -1,7 +1,7 @@
 
 default[:pig][:home_dir]          = '/usr/lib/pig'
 
-default[:pig][:version]           = "0.9.1"
+default[:pig][:version]           = "0.9.2"
 default[:pig][:release_url]       = ":apache_mirror:/pig/pig-:version:/pig-:version:.tar.gz"
 default[:java][:java_home]        = '/usr/lib/jvm/java-6-sun/jre'
 
