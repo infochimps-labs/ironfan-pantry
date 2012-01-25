0ff9c87c5206d14cfba56a6e3e5cf8a433d27601
Philip (flip) Kromer <flip@infochimps.org>
2012-01-25 15:27:07 -0600

    Snappy codec. NOTE: not needed for HBase / Flume / Hadoop (those should use the native libs as provided by the hadoop_cluster cookbook)

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default[:snappy][:home_dir]          = '/usr/local/share/snappy'
+
+default[:snappy][:version]           = "1.0.4"
+default[:snappy][:release_url]       = "http://snappy.googlecode.com/files/snappy-:version:.tar.gz"
