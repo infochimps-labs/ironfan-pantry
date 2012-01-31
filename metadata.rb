61275945f32932cb59031048344e82dcccf7ee23
Philip (flip) Kromer <flip@infochimps.org>
2012-01-30 23:26:34 -0600

    culled extraneous variable declarations as recommended by linter

--- metadata.rb
+++ metadata.rb
@@ -17,11 +17,6 @@ recipe           "resque::dedicated_redis",            "Dedicated redis -- a red
   supports os
 end
 
-attribute "resque/dir",
-  :display_name          => "",
-  :description           => "",
-  :default               => "/data/db/resque"
-
 attribute "resque/log_dir",
   :display_name          => "",
   :description           => "",
@@ -52,11 +47,6 @@ attribute "resque/db_basename",
   :description           => "",
   :default               => "resque_queue.rdb"
 
-attribute "resque/cluster_name",
-  :display_name          => "",
-  :description           => "",
-  :default               => "cluster_name"
-
 attribute "resque/namespace",
   :display_name          => "",
   :description           => "",
@@ -71,57 +61,6 @@ attribute "resque/group",
   :display_name          => "",
   :description           => "",
   :default               => "resque"
-
-attribute "resque/queue_addr",
-  :display_name          => "",
-  :description           => "",
-  :default               => "10.20.30.40"
-
-attribute "resque/queue_port",
-  :display_name          => "",
-  :description           => "",
-  :default               => "6388"
-
-attribute "resque/dashboard_port",
-  :display_name          => "",
-  :description           => "",
-  :default               => "6389"
-
-attribute "resque/redis_client_timeout",
-  :display_name          => "",
-  :description           => "",
-  :default               => "300"
-
-attribute "resque/redis_glueoutputbuf",
-  :display_name          => "",
-  :description           => "",
-  :default               => "yes"
-
-attribute "resque/redis_vm_enabled",
-  :display_name          => "",
-  :description           => "",
-  :default               => "yes"
-
-attribute "resque/redis_vm_max_memory",
-  :display_name          => "",
-  :description           => "",
-  :default               => "128m"
-
-attribute "resque/redis_vm_pages",
-  :display_name          => "",
-  :description           => "",
-  :default               => "16777216"
-
-attribute "resque/redis_saves",
-  :display_name          => "",
-  :description           => "",
-  :type                  => "array",
-  :default               => [["900", "1"], ["300", "10"], ["60", "10000"]]
-
-attribute "resque/redis_slave",
-  :display_name          => "",
-  :description           => "",
-  :default               => "no"
 
 attribute "resque/app_env",
   :display_name          => "",
