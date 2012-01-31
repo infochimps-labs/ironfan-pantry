49cf599da1c0d6ccab3a2f86edcd1d6df3cc27ba
Philip (flip) Kromer <flip@infochimps.org>
2012-01-31 00:42:01 -0600

    bump chef versions of cookbooks

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "37signals"
 maintainer_email "sysadmins@37signals.com"
 license          ""
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.3"
+version          "3.0.4"
 
 description      "NFS: network shared filesystem"
 
