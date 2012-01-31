8c4c4469c337d6f931661e4a5e45294b0a922cf0
Philip (flip) Kromer <flip@infochimps.org>
2012-01-30 23:26:34 -0600

    culled extraneous variable declarations as recommended by linter

--- metadata.rb
+++ metadata.rb
@@ -20,6 +20,7 @@ recipe           "flume::jruby_plugin",                "Jruby Plugin"
 recipe           "flume::master",                      "Configures Flume Master, installs and starts service"
 recipe           "flume::node",                        "Configures Flume Node, installs and starts service"
 recipe           "flume::test_flow",                   "Test Flow"
+recipe           "flume::config",                      "Finalizes the config, writes out the config files"
 
 %w[ debian ubuntu ].each do |os|
   supports os
