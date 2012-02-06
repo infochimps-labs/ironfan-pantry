d1019d7cfa37bd8d3ec98310a2e4c15103c8e650
Dhruv Bansal <dhruv@ph.utexas.edu>
2012-02-06 02:40:10 -0600

    added lwrp for zabbix applications

---
+++ providers/application.rb
@@ -0,0 +1,20 @@
+include Chef::RubixConnection
+
+action :create do
+  return unless connected_to_zabbix? && self.zabbix_host
+  Rubix::Application.find_or_create(:host_id => self.zabbix_host.id, :name => new_resource.name)
+end
+
+action :destroy do
+  return unless connected_to_zabbix? && self.zabbix_host
+  app = Rubix::Application.find(:host_id => self.zabbix_host.id, :name => new_resource.name)
+  app.destroy if app
+end
+
+attr_accessor :zabbix_host
+
+def load_current_resource
+  return unless connect_to_zabbix_server(new_resource.server)
+  self.zabbix_host = Rubix::Host.find(:name => new_resource.host)
+  Chef::Log.error("Cannot find a Zabbix host named #{new_resource.host}") unless self.zabbix_host
+end
