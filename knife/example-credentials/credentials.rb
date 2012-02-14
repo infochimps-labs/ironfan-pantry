700dd7295060e38d73d6c11a7c57662ee90721b3
Philip (flip) Kromer <flip@infochimps.org>
2012-02-14 10:28:05 -0600

    moved cookbooks to cookbooks/ as this is now the thing in vendor/

--- knife/example-credentials/credentials.rb
+++
@@ -1,3 +0,0 @@
-Chef::Config.knife[:aws_access_key_id]      = "GET_FROM_AWS_CONSOLE AND SET IN #{__FILE__}"
-Chef::Config.knife[:aws_secret_access_key]  = "GET_FROM_AWS_CONSOLE AND SET IN #{__FILE__}"
-Chef::Config.knife[:aws_account_id]         = "GET_FROM_AWS_CONSOLE AND SET IN #{__FILE__}"
