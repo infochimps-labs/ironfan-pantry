61f92fd72024d89e0d319da8400cf5472477086b
Dhruv Bansal <dhruv@ph.utexas.edu>
2012-02-17 15:43:07 -0600

    added user, user_group, and action LWRPs

--- cookbooks/zabbix/recipes/database_mysql.rb
+++ cookbooks/zabbix/recipes/database_mysql.rb
@@ -22,7 +22,8 @@
 include_recipe "mysql::client"
 
 # Establish root MySQL connection
-root_mysql_conn = {:host => node.zabbix.database.host, :username => node.zabbix.database.root_user, :password => node.zabbix.database.root_password}
+root_mysql_conn    = {:host => node.zabbix.database.host, :username => node.zabbix.database.root_user, :password => node.zabbix.database.root_password}
+base_mysql_command = "/usr/bin/mysql -h #{node.zabbix.database.host} -u #{node.zabbix.database.root_user} #{node.zabbix.database.name} -p#{node.zabbix.database.root_password}"
 
 # Ensure we have the mysql gem available at *compile* time.
 begin
@@ -40,8 +41,8 @@ rescue LoadError
 end
 
 # Only execute if database is missing...
-m=Mysql.new(node.zabbix.database.host,node.zabbix.database.root_user,node.zabbix.database.root_password)
-if m.list_dbs.include?(node.zabbix.database.name) == false
+mysql_connection = Mysql.new(node.zabbix.database.host,node.zabbix.database.root_user,node.zabbix.database.root_password)
+if mysql_connection.list_dbs.include?(node.zabbix.database.name) == false
 
   # Create Zabbix database
   mysql_database node.zabbix.database.name do
@@ -61,7 +62,7 @@ if m.list_dbs.include?(node.zabbix.database.name) == false
   end
   
   # Populate Zabbix database
-  populate_command = "/usr/bin/mysql -h #{node.zabbix.database.host} -u #{node.zabbix.database.root_user} #{node.zabbix.database.name} -p#{node.zabbix.database.root_password} < /opt/zabbix-#{node.zabbix.server.version}"
+  populate_command = "#{base_mysql_command} < /opt/zabbix-#{node.zabbix.server.version}"
   execute "zabbix_populate_schema" do
     command "#{populate_command}/create/schema/mysql.sql"
     action :nothing
@@ -86,4 +87,46 @@ mysql_database_user node.zabbix.database.user do
   database_name node.zabbix.database.name
   privileges    [:select,:update,:insert,:create,:drop,:delete]
   action        :grant
+end
+
+# Create a Zabbix "Admin" user, an "API access" group, and ensure the
+# super user is in the API access group.  This lets us
+ruby_block "zabbix_ensure_super_admin_user_with_api_access" do
+  block do
+    username   = node.zabbix.api.username
+    first_name = 'Zabbix'
+    last_name  = 'Administrator'
+    md5        = Digest::MD5.hexdigest(node.zabbix.api.password)
+    rows       = 200
+    type       = 3
+    grp_name   = node.zabbix.api.user_group
+    api_access = 1
+
+    mysql_connection.query(%Q{USE #{node.zabbix.database.name}})
+    
+    existing_users = mysql_connection.query(%Q{SELECT userid FROM users WHERE `alias`="#{username}"})
+    if existing_users.num_rows == 0
+      mysql_connection.query(%Q{INSERT INTO users (alias, name, surname, passwd, rows_per_page, type) VALUES ("#{username}", "#{first_name}", "#{last_name}", "#{md5}", #{rows}, #{type})})
+      userid = mysql_connection.query(%Q{SELECT userid FROM users WHERE `alias`="#{username}"}).fetch_row.first.to_i
+    else
+      userid = existing_users.fetch_row.first.to_i
+      mysql_connection.query(%Q{UPDATE users SET `alias`="#{username}", name="#{first_name}", surname="#{last_name}", passwd="#{md5}", rows_per_page=#{rows}, type=#{type} WHERE userid="#{userid}"})
+    end
+
+    existing_groups = mysql_connection.query(%Q{SELECT usrgrpid FROM usrgrp WHERE name="#{grp_name}"})
+    if existing_groups.num_rows == 0
+      mysql_connection.query(%Q{INSERT INTO usrgrp (name, api_access) VALUES ("#{grp_name}", #{api_access})})
+      usrgrpid = mysql_connection.query(%Q{SELECT usrgrpid FROM usrgrp WHERE name="#{grp_name}"}).fetch_row.first.to_i
+    else
+      usrgrpid = existing_groups.fetch_row.first.to_i
+      mysql_connection.query(%Q{UPDATE usrgrp SET name="#{grp_name}", api_access=#{api_access} WHERE usrgrpid=#{usrgrpid}})
+    end
+
+    existing_join = mysql_connection.query(%Q{SELECT id FROM users_groups WHERE usrgrpid=#{usrgrpid} AND userid=#{userid}})
+    if existing_join.num_rows == 0
+      mysql_connection.query(%Q{INSERT INTO users_groups (usrgrpid, userid) VALUES (#{usrgrpid}, #{userid})})
+    end
+
+    mysql_connection.query(%Q{DELETE FROM users WHERE alias='admin'});
+  end
 end
---
+++ cookbooks/zabbix/resources/user_group.rb
@@ -0,0 +1,14 @@
+actions :create, :destroy
+
+attribute :server,             :kind_of  => String, :default => 'localhost'
+
+attribute :users,      :kind_of  => Array, :default => []
+attribute :api_access, :equal_to => [true, false]
+attribute :debug_mode, :equal_to => [true, false]
+attribute :banned,     :equal_to => [true, false]
+attribute :gui_access, :equal_to => [:default, :internal, :disabled]
+
+def initialize *args
+  super
+  @action = :create
+end
