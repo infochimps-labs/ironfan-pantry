0edec09f959649028f48f1be69b355b80595af41
Dhruv Bansal <dhruv@ph.utexas.edu>
2012-02-27 13:17:52 -0600

    rubix bump to 0.5.2

--- cookbooks/zabbix/libraries/rubix_connection.rb
+++ cookbooks/zabbix/libraries/rubix_connection.rb
@@ -3,7 +3,7 @@ class Chef
   module RubixConnection
 
     # The version of Rubix we expect to use.
-    RUBIX_VERSION = '0.5.1'
+    RUBIX_VERSION = '0.5.2'
 
     # For a pool of shared connections to Zabbix API servers.
     CONNECTIONS = {  }
