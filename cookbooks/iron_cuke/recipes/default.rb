98692a83fb248afd64bf1353b673e348f8c4fbc4
Dieterich Lawson <dieterich.lawson@gmail.com>
2012-07-20 15:36:02 -0500

    Ironcuke refactorings

--- cookbooks/iron_cuke/recipes/default.rb
+++ cookbooks/iron_cuke/recipes/default.rb
@@ -2,12 +2,15 @@
 # Cookbook Name:: iron_cuke
 # Recipe:: default
 #
-
-directory node['iron_cuke']['conf_dir'] do
-  owner node['iron_cuke']['user']
-  group node['iron_cuke']['group']
-  mode 0755
+standard_dirs('iron_cuke') do
+  directories [:conf_dir]
 end
+
+#directory node['iron_cuke']['conf_dir'] do
+  #  owner node['iron_cuke']['user']
+  #  group node['iron_cuke']['group']
+  #  mode 0755
+  #end
 
 git node['iron_cuke']['home_dir'] do
   repository node['iron_cuke']['git_repo']
@@ -17,8 +20,6 @@ end
 
 file "#{node['iron_cuke']['conf_dir']}/announces.json" do
   content node['announces'].to_json
-  owner node['iron_cuke']['user']
-  group node['iron_cuke']['group']
 end
 
 execute "bundle install" do
@@ -27,8 +28,7 @@ end
 
 execute "bundle exec bin/ironcuke gen_tests -a #{node['iron_cuke']['conf_dir']}/announces.json" do
   cwd node['iron_cuke']['home_dir']
-end
-
-execute "bundle exec bin/ironcuke judge" do
-  cwd node['iron_cuke']['home_dir']
+  # only run when announces.json changes
+  subscribes  :run, resources(:file => "#{node['iron_cuke']['conf_dir']}/announces.json")
+  action :nothing
 end
