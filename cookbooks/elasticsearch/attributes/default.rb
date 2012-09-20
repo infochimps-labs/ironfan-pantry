6e183ba148df512801a44fd861b347b9470a564d
Josh Bronson <josh.bronson@gmail.com>
2012-09-20 10:45:56 -0500

    turning default log level for elasticsearch down to INFO... DEBUG spews gigs of nonsense

--- cookbooks/elasticsearch/attributes/default.rb
+++ cookbooks/elasticsearch/attributes/default.rb
@@ -86,7 +86,7 @@ default[:tuning][:ulimit]['@elasticsearch'] = { :nofile => { :both => 32768 }, :
 
 # most of the log lines are manageable at level 'DEBUG'
 # the voluminous ones are broken out separately
-default[:elasticsearch][:log_level][:default]         = 'DEBUG'
+default[:elasticsearch][:log_level][:default]         = 'INFO'
 default[:elasticsearch][:log_level][:index_store]     = 'INFO'  # lots of output but might be useful
 default[:elasticsearch][:log_level][:action_shard]    = 'INFO'  # lots of output but might be useful
 default[:elasticsearch][:log_level][:cluster_service] = 'INFO'  # lots of output but might be useful
