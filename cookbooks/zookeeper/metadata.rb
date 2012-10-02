10caa1289f7fb2214733dcd1a32d1df38a7fae93
Logan Lowell <fractaloop@gmail.com>
2012-10-02 15:33:38 -0500

    ZK Version bump

--- cookbooks/zookeeper/metadata.rb
+++ cookbooks/zookeeper/metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "Chris Howe - Infochimps, Inc"
+maintainer       "Infochimps, Inc"
 maintainer_email "coders@infochimps.com"
 license          "Apache 2.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.1.3"
+version          "3.1.4"
 
 description      "Zookeeper, a distributed high-availability consistent datastore"
 
