fac7d421be051c816c58aa93d94a6828143b88bd
Nathaniel Eliot <temujin9@t9productions.com>
2012-12-12 18:23:56 -0600

    jenkins_integration: further definition cleanup

--- cookbooks/jenkins_integration/definitions/ironfan_homebase.rb
+++ cookbooks/jenkins_integration/definitions/ironfan_homebase.rb
@@ -2,15 +2,22 @@
 #   may be used to test Ironfan deployment
 
 define(:ironfan_homebase,
-  :user         => node[:jenkins_integration][:user],
-  :group        => node[:jenkins_integration][:group],
-  :path         => node[:jenkins_integration][:ironfan_homebase][:path] + '/' + params[:name],
-  :repository   => node[:jenkins_integration][:ironfan_homebase][:repository]
+  :user         => nil,         # User to clone as
+  :group        => nil,         # Group to clone as
+  :path         => nil,         # Path to clone to
+  :repository   => nil          # Repository to clone from
   ) do
 
+  defaults              = {}
+  defaults[:user]       = node[:jenkins_integration][:user]
+  defaults[:group]      = node[:jenkins_integration][:group]
+  defaults[:path]       = node[:jenkins_integration][:ironfan_homebase][:path] + '/' + params[:name]
+  defaults[:repository] = node[:jenkins_integration][:ironfan_homebase][:repository]
+  config                = defaults.merge(params)
+
   git_private_repo params[:name] do
-    path                   params[:path]
-    repository             params[:repository]
+    path                   config[:path]
+    repository             config[:repository]
 #     private_keys_contents  node[:ironfan_api][:homebase][:private_keys]
     action                 :sync
     enable_submodules      true
--- cookbooks/jenkins_integration/metadata.rb
+++ cookbooks/jenkins_integration/metadata.rb
@@ -3,5 +3,7 @@ maintainer_email "temujin9@infochimps.com"
 license          "Apache 2.0"
 description      "Installs/Configures jenkins_integration"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-long_description IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
+recipe           "jenkins_integration::default",        "Base of jenkins_integration"
+recipe           "jenkins_integration::ironfan_ci",     "Ironfan Continuous Integration"
