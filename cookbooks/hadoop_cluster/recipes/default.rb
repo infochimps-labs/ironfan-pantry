f31fa9fa513844310adb78c744a32731cde660a1
Nathaniel Eliot <temujin9@t9productions.com>
2012-12-18 14:07:57 -0600

    hadoop_cluster: Cleaning up bare node assignment per Chef 10.16.2 deprecation warning

--- cookbooks/hadoop_cluster/recipes/default.rb
+++ cookbooks/hadoop_cluster/recipes/default.rb
@@ -121,7 +121,7 @@ end
 %w[jobtracker tasktracker       ].each{|svc| directory("#{node[:hadoop][:log_dir]}/#{svc}"){ action(:create) ; owner 'mapred' ; group "hadoop"; mode "0775" } }
 
 # JMX should listen on the public interface
-node[:hadoop][:jmx_dash_addr] = public_ip_of(node)
+node.set[:hadoop][:jmx_dash_addr] = public_ip_of(node)
 
 # Make /var/log/hadoop point to the logs (which is on the first scratch dir),
 # and /var/run/hadoop point to the actual pid dir
@@ -129,18 +129,18 @@ force_link("/var/log/hadoop",                    node[:hadoop][:log_dir] )
 force_link("/var/log/#{node[:hadoop][:handle]}", node[:hadoop][:log_dir] )
 force_link("/var/run/#{node[:hadoop][:handle]}", node[:hadoop][:pid_dir] )
 
-node[:hadoop][:exported_jars] = [
+node.set[:hadoop][:exported_jars] = [
   "#{node[:hadoop][:home_dir]}/hadoop-core.jar",
   "#{node[:hadoop][:home_dir]}/hadoop-examples.jar",
   "#{node[:hadoop][:home_dir]}/hadoop-test.jar",
   "#{node[:hadoop][:home_dir]}/hadoop-tools.jar",
 ]
 
-node[:hadoop][:exported_libs] = Dir["#{node[:hadoop][:home_dir]}/lib/native/**/*.*"].sort.reject{|ff| File.directory?(ff) }
+node.set[:hadoop][:exported_libs] = Dir["#{node[:hadoop][:home_dir]}/lib/native/**/*.*"].sort.reject{|ff| File.directory?(ff) }
 
 Chef::Log.debug( [ 'hadoop native libs', node[:hadoop][:exported_libs] ].inspect )
 
-node[:hadoop][:exported_confs]  = [
+node.set[:hadoop][:exported_confs]  = [
   "#{node[:hadoop][:conf_dir]}/core-site.xml",
   "#{node[:hadoop][:conf_dir]}/hdfs-site.xml",
   "#{node[:hadoop][:conf_dir]}/mapred-site.xml",
