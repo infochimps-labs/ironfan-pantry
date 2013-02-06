d7e80a1230a72a5e662dc12a0c74b4b27a734f7a
Nathaniel Eliot <temujin9@t9productions.com>
2013-02-06 15:25:38 -0600

    jenkins_integration: reworked to do all repositories as subdirectories of workspace

--- cookbooks/jenkins_integration/attributes/default.rb
+++ cookbooks/jenkins_integration/attributes/default.rb
@@ -3,10 +3,10 @@ default[:jenkins_integration][:git][:email]     = 'jenkins@example.org'
 
 default[:jenkins_integration][:security]        = 'local_users'
 
-default[:jenkins_integration][:ironfan_ci][:repository]         = 'git@github.com:infochimps-labs/ironfan-homebase.git'
+default[:jenkins_integration][:ironfan_ci][:test_homebase]      = 'ironfan-homebase'
 default[:jenkins_integration][:ironfan_ci][:chef_user]          = 'test'
 default[:jenkins_integration][:ironfan_ci][:target]             = 'testharness-simple'
-default[:jenkins_integration][:ironfan_ci][:broken]             = nil   # Set to launch a known-broken instance
+default[:jenkins_integration][:ironfan_ci][:broken]             = nil   # Set to launch a known-broken facet
 default[:jenkins_integration][:ironfan_ci][:branch]             = 'master'
 
 ironfan_pantry = Mash.new
@@ -14,8 +14,12 @@ ironfan_pantry[:project]        = 'https://github.com/infochimps-labs/ironfan-pa
 ironfan_pantry[:repository]     = 'git@github.com:infochimps-labs/ironfan-pantry.git'
 default[:jenkins_integration][:ironfan_ci][:pantries]['ironfan-pantry'] = ironfan_pantry
 
-# Build homebases as above
 default[:jenkins_integration][:ironfan_ci][:homebases]          = {}
+## Build homebase entries like so (but not in default)
+# ironfan_homebase = Mash.new
+# ironfan_homebase[:project]      = 'https://github.com/infochimps-labs/ironfan-homebase/'
+# ironfan_homebase[:repository]   = 'git@github.com:infochimps-labs/ironfan-homebase.git'
+# default[:jenkins_integration][:ironfan_ci][:homebases]['ironfan-homebase'] = ironfan_homebase
 
 # Plugins that we need for the Ironfan CI job to work correctly
 node.default[:jenkins][:server][:plugins] += %w[ join parameterized-trigger ]
