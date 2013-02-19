2a08a37db50fb30b1aa4603019e7c0b2d473b353
Nathaniel Eliot <temujin9@t9productions.com>
2013-02-18 18:54:07 -0600

    vsftp: switching to VERSION framework

--- cookbooks/vsftpd/metadata.rb
+++ cookbooks/vsftpd/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dhruv@infochimps.com"
 license          "Apache License, Version 2.0"
 description      "Installs/Configures vsftpd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.5.0"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
 depends 'silverware'
 depends 'volumes'
