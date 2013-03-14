dbdcad00a126a04d0e3367f0677b8f1c4c4ba3c1
Nathaniel Eliot <temujin9@t9productions.com>
2013-03-14 15:05:12 -0500

    elasticsearch: can't use periods in symbols, use underscores instead

--- cookbooks/elasticsearch/attributes/default.rb
+++ cookbooks/elasticsearch/attributes/default.rb
@@ -67,7 +67,7 @@ default[:elasticsearch][:flush_threshold_size]    = "200mb"
 default[:elasticsearch][:flush_threshold_period]  = "60s"
 default[:elasticsearch][:cache_filter_size]       = "20%"  # can be a percent ("10%") or a number ("128m")
 default[:elasticsearch][:index_buffer_size]       = "10%"  # can be a percent ("10%") or a number ("128m") (changed 2012-10 to default 10%, same as es default)
-default[:elasticsearch][:index.cache.field.type]  = resident    # Another option is soft, which will increase cache performance but be weary because this will make it crash if the "wrong" query is issued. The field cache can OOM your machine in a moment when too large, but when you have the RAM field values are one of the most important things to cache. Consider setting this to a very high value to be safe; certainly, set it on indexes that are very large.
+default[:elasticsearch][:index_cache_field_type]  = resident    # Another option is soft, which will increase cache performance but be weary because this will make it crash if the "wrong" query is issued. The field cache can OOM your machine in a moment when too large, but when you have the RAM field values are one of the most important things to cache. Consider setting this to a very high value to be safe; certainly, set it on indexes that are very large.
 default[:elasticsearch][:merge_factor]            = 10
 default[:elasticsearch][:floor_segment]           = "2.7mb"
 
