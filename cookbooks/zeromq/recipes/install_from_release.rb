2b355daf36f2ba2671b331853095b7f45bea6064
Brandon Bell <brandon@MegaDinosaur.(none)>
2013-04-09 13:53:54 -0500

    Adding RHEL package dependency

--- cookbooks/zeromq/recipes/install_from_release.rb
+++ cookbooks/zeromq/recipes/install_from_release.rb
@@ -1,5 +1,7 @@
 include_recipe 'install_from'
 
+package 'libuuid-devel' if platform?('redhat', 'centos')
+
 install_from_release('zeromq') do
   release_url   node[:zeromq][:release_url]
   version       node[:zeromq][:version]
