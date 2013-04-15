3cc82f2912ad6beb33c44d0262a3fd322b10769e
Nathaniel Eliot <temujin9@t9productions.com>
2013-04-15 13:30:08 -0500

    ironfan-ci: obselete cookbook, removing (see jenkins_integration)

--- cookbooks/ironfan-ci/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Infochimps"
-maintainer_email "infochimps@infochimps.com"
-license          "All rights reserved"
-description      "Installs/Configures Ironfan CI framework"
-# long_description IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
