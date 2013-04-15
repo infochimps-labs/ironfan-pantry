58142626938717846f7912434522ec6bf318f897
Nathaniel Eliot <temujin9@t9productions.com>
2013-04-15 15:27:17 -0500

    Moving cookbooks to community-pantry (chef-client, cloud_utils, daemontools, rvm, snappy, strongswan, sudo, ubuntu, ucspi-tcp, unicorn, volumes_ebs)

--- cookbooks/snappy/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default[:snappy][:home_dir]          = '/usr/local/share/snappy'
-
-default[:snappy][:version]           = "1.0.4"
-default[:snappy][:release_url]       = "http://snappy.googlecode.com/files/snappy-:version:.tar.gz"
--- cookbooks/strongswan/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Jerry Jackson"
-maintainer_email "jerry.w.jacson@gmail.com"
-license          "All rights reserved"
-description      "Installs/Configures strongswan"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
-
-depends 'route53'
