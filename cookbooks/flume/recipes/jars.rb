73bbb7e212e0f6525dc8a71ed270b651369e42ae
Nathaniel Eliot <temujin9@t9productions.com>
2013-04-15 13:05:43 -0500

    moving cassandra, flume, ganglia, graphite, resque, statsd, openssh, rstats to community-pantry

--- cookbooks/flume/recipes/jars.rb
+++
@@ -1,7 +0,0 @@
-%w[commons-io-1.3.2.jar json-20090211.jar mysql-connector-java-5.1.18-bin.jar].each do |jarname|
-  cookbook_file "#{node[:flume][:home_dir]}/lib/#{jarname}" do
-    source jarname
-    owner "flume"
-    mode  "0644"
-  end
-end
