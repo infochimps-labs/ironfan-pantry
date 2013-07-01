221b130186c8e609dadade2ee76a3a619fda2535
Nathaniel Eliot <temujin9@t9productions.com>
2013-07-01 15:27:33 -0500

    cube: moved back to ironfan-enterprise (ref. https://github.com/infochimps-labs/ironfan/issues/301)

--- cookbooks/cube/recipes/warmer.rb
+++
@@ -1,7 +0,0 @@
-announce(:cube, :warmer,
-         :logs    => {:warmer => node[:cube][:log_dir] },
-         :daemons => {:http => { :name => 'node', :user => node[:cube][:user], :cmd => 'warmer' } })
-
-runit_service "cube_warmer" do
-  options       Mash.new().merge(node[:cube]).merge(node[:cube][:warmer])
-end
