4abe929b90fe0a65f7f0d6b0d46d2edfa0951da2
Brandon Bell <brandon@MegaDinosaur.(none)>
2013-07-22 12:13:31 -0500

    removing chef run for elasticserach

--- cookbooks/ebs_migrate/attributes/default.rb
+++ cookbooks/ebs_migrate/attributes/default.rb
@@ -8,10 +8,11 @@ default[:ebs_migrate][:target] = nil      # The target cluster and facet you hav
 # Elasticsearch
 #
 
-default[:ebs_migrate][:es][:mount_point] = nil # This is the mount point of the above ebs volume.  FIXME 
-default[:ebs_migrate][:es][:device]      = nil # Device the volume is attached as. This is for recovery purposes in failure  FIXME 
-default[:ebs_migrate][:es][:minute]      = 15 
-default[:ebs_migrate][:es][:hour]        = 02
-default[:ebs_migrate][:es][:day]         = '*' 
-default[:ebs_migrate][:es][:month]       = '*'
-default[:ebs_migrate][:es][:weekday]     = '*'
+default[:ebs_migrate][:es][:mount_point]   = nil # This is the mount point of the above ebs volume.  FIXME 
+default[:ebs_migrate][:es][:device]        = nil # Device the volume is attached as. This is for recovery purposes in failure  FIXME 
+default[:ebs_migrate][:es][:mount_cmd]     = nil # Mount command to run before running chef client. This prevents chef-client timeouts FIXME
+default[:ebs_migrate][:es][:minute]        = 15 
+default[:ebs_migrate][:es][:hour]          = 02
+default[:ebs_migrate][:es][:day]           = '*' 
+default[:ebs_migrate][:es][:month]         = '*'
+default[:ebs_migrate][:es][:weekday]       = '*'
