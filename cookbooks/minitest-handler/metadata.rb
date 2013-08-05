6beab5093bb04e30c256e0d90054cba8b73783ca
Brandon Bell <brandon@MegaDinosaur.(none)>
2013-08-05 17:17:57 -0500

    adding version file

--- cookbooks/minitest-handler/metadata.rb
+++ cookbooks/minitest-handler/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "bryan.berry@gmail.com"
 license          "Apache 2.0"
 description      "Installs and configures minitest-chef-handler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
 depends "chef_handler"
