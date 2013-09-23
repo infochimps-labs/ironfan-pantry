8039e9b2bb39b3794b3cca8c993ad227b4bc44bf
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-09-23 18:37:03 -0500

    only clean 'infochimps' topic events by default

--- cookbooks/vayacondios/recipes/cleaner.rb
+++ cookbooks/vayacondios/recipes/cleaner.rb
@@ -12,8 +12,11 @@ include_recipe("vayacondios::install_from_git")
 mongo = discover(:mongodb, :server)
 Chef::Log.warn("No MongoDB database is available to clean (didn't set and couldn't discover node[:vayacondios][:mongodb][:database])") unless node[:vayacondios][:mongodb][:database]
 
+# FIXME -- for now we're hard-coding to delete all events on the
+# 'infochimps' topic from the last hour.  This needs to be more
+# generally configurable if it is included at all.
 cron "Clean old Vayacondios events" do
   minute  "*/10"
   path    "/bin:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin"
-  command "cd #{node[:vayacondios][:home_dir]} && bundle exec vcd-clean --host=#{mongo.private_ip} --port=#{mongo.ports[:http][:port]} --database=#{node[:vayacondios][:mongodb][:database]} --upto=#{node[:vayacondios][:cleaner][:max_age]} >> #{node[:vayacondios][:log_dir]}/cleaner.log 2>&1"
+  command "cd #{node[:vayacondios][:home_dir]} && bundle exec vcd-clean --host=#{mongo.private_ip} --port=#{mongo.ports[:http][:port]} --database=#{node[:vayacondios][:mongodb][:database]} --upto=#{node[:vayacondios][:cleaner][:max_age]} --matching='^infochimps\..*\.events$' >> #{node[:vayacondios][:log_dir]}/cleaner.log 2>&1"
 end
