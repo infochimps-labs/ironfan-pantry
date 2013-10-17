9f2759b23a7d7d670c04858fc9379a9454fe8c07
Erik Mackdanz <erik@infochimps.com>
2013-10-17 16:20:51 -0500

    namenode deb is installed, missing some config

--- cookbooks/hadoop/metadata.rb
+++ cookbooks/hadoop/metadata.rb
@@ -1,7 +1,10 @@
 name             'hadoop'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures hadoop'
+maintainer       "Philip (flip) Kromer - Infochimps, Inc"
+maintainer_email "coders@infochimps.com"
+license          "Apache 2.0"
+description      "Hadoop: distributed massive-scale data processing framework. Store and analyze terabyte-scale datasets with ease"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+
+conflicts        'hadoop_cluster'
+depends          'runit'
