c0371d723784635647efec6d202f49ca09aaacc0
Rachel McCuistion <rachel@infochimps.com>
2014-02-10 14:39:45 -0600

    first stab at volume_dirs for storm logs

--- cookbooks/storm/recipes/default.rb
+++ cookbooks/storm/recipes/default.rb
@@ -5,3 +5,12 @@
 #
 
 include_recipe 'silverware'
+
+# Storm log storage on a single scratch dir
+volume_dirs('storm.log') do
+  type          :local
+  selects       :single
+  path          'storm/log'
+  group         'storm'
+  mode          "0777"
+end
