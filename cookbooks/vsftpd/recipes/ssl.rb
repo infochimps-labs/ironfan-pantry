d707051c28ffb2de83d4a5e9aaff98c82996d1bc
Zarko Bizaca <zarko@infochimps.com>
2014-03-04 14:19:59 -0600

    feature: FTP loader
    Added FTP Loader to kafka::contrib
    Each ftp site is monitored by a cron job.  There is an exapmle
    for testing .
    
    Fixsed a bug in vsftpd/recipes/ssl.rb
    Added UTF-8 encoding to lftp/metadata.rb

--- cookbooks/vsftpd/recipes/ssl.rb
+++ cookbooks/vsftpd/recipes/ssl.rb
@@ -1,30 +1,34 @@
-node.default[:vsftpd][:ssl_cert_path] = File.join(node[:vsftpd][:conf_dir], 'vsftpd.pem')
-if node[:vsftpd][:ssl_cert]
-  file node[:vsftpd][:ssl_cert_path] do
-    owner   'root'
-    group   'root'
-    mode    '0600'
-    action  :create
-    content node[:vsftpd][:ssl_cert]
-  end
-else
-  file node[:vsftpd][:ssl_cert_path] do
-    owner   'root'
-    group   'root'
-    mode    '0600'
-    action  :create
-    source  'ftp.example.com.pem'
-  end
-end
+if node[:vsftpd][:ssl_enable]
+  
+  node.default[:vsftpd][:ssl_cert_path] = File.join(node[:vsftpd][:conf_dir], 'vsftpd.pem')
 
-if node[:vsftpd][:ssl_private_key]
-  node.default[:vsftpd][:ssl_private_key_path] = File.join(node[:vsftpd][:conf_dir], 'vsftpd.key')
-  remote_file node[:vsftpd][:ssl_private_key_path] do
-    owner   'root'
-    group   'root'
-    mode    '0600'
-    action  :create
-    source  "ftp.example.com.key"
-    content node[:vsftpd][:ssl_private_key]
+  if node[:vsftpd][:ssl_cert]
+    file node[:vsftpd][:ssl_cert_path] do
+      owner   'root'
+      group   'root'
+      mode    '0600'
+      action  :create
+      content node[:vsftpd][:ssl_cert]
+    end
+  else
+    remote_file node[:vsftpd][:ssl_cert_path] do
+      owner   'root'
+      group   'root'
+      mode    '0600'
+      action  :create
+      source  'file://ftp.example.com.pem'
+    end
+  end
+
+  if node[:vsftpd][:ssl_private_key]
+    node.default[:vsftpd][:ssl_private_key_path] = File.join(node[:vsftpd][:conf_dir], 'vsftpd.key')
+    remote_file node[:vsftpd][:ssl_private_key_path] do
+      owner   'root'
+      group   'root'
+      mode    '0600'
+      action  :create
+      source  "ftp.example.com.key"
+      content node[:vsftpd][:ssl_private_key]
+    end
   end
 end
