a1ffd615873b2c29c1f1829c35d51e69769710aa
Huston Hoburg <huston@infochimps.com>
2014-03-10 17:30:23 -0500

    Update vayacondios to support v3 configuration

--- cookbooks/vayacondios/attributes/default.rb
+++ cookbooks/vayacondios/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:vayacondios][:git_url]          = "https://github.com/infochimps-labs/vayacondios.git"
-default[:vayacondios][:git_version]      = 'master'
+default[:vayacondios][:git_version]      = '0.3.0'
 default[:vayacondios][:deploy_strategy]  = :deploy
 
 default[:vayacondios][:organization] = nil
