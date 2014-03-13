c820dcd406360ab5ca9d294813be785232399c57
Travis Dempsey <travis@infochimps.com>
2014-03-13 10:18:41 -0500

    feature: New Boswell Implementation
    Linked to branch of the same name in ironfan-enterprise
    
    Squashed commit of the following:
    
    commit c508aa34abceddb82db0d42e5b39cc544379688c
    Author: Travis Dempsey <dempsey.travis@gmail.com>
    Date:   Thu Mar 13 10:16:52 2014 -0500
    
        vcd client needs to install conf in a particular place
    
        Change-Id: I496d16c7271c031658718bfd85ed9effeec74b22
    
    commit 093f762f8ff023e8edeb4f20ec220aeade3d4098
    Author: Travis Dempsey <dempsey.travis@gmail.com>
    Date:   Thu Mar 13 10:15:46 2014 -0500
    
        remove line preventing nodes from discovering themselves
    
        Change-Id: I9e2760820cb5b48455852dd467b0843038f799c8
    
    Change-Id: Iad73177c7719af73987329082e9ca0e151b801f7

--- cookbooks/vayacondios/attributes/default.rb
+++ cookbooks/vayacondios/attributes/default.rb
@@ -1,27 +1,29 @@
-default[:vayacondios][:git_url]          = "https://github.com/infochimps-labs/vayacondios.git"
-default[:vayacondios][:git_version]      = '0.3.0'
-default[:vayacondios][:deploy_strategy]  = :deploy
+default[:vayacondios][:git_url]               = "https://github.com/infochimps-labs/vayacondios.git"
+default[:vayacondios][:git_version]           = '0.3.0'
+default[:vayacondios][:deploy_strategy]       = :deploy
 
-default[:vayacondios][:organization] = nil
+default[:vayacondios][:organization]          = nil
 
-default[:vayacondios][:user]       = 'www-data'
-default[:vayacondios][:group]      = 'webservers'
+default[:vayacondios][:user]                  = 'www-data'
+default[:vayacondios][:group]                 = 'webservers'
 
-default[:vayacondios][:deploy_root]  = '/var/www/vayacondios'
-default[:vayacondios][:home_dir]     = '/var/www/vayacondios/current'
-default[:vayacondios][:conf_dir]     = '/var/www/vayacondios/shared/config'
-default[:vayacondios][:log_dir]      = '/var/www/vayacondios/shared/log'
-default[:vayacondios][:tmp_dir]      = '/var/www/vayacondios/shared/tmp'
-default[:vayacondios][:pid_dir]      = '/var/www' # it's www-data
+default[:vayacondios][:deploy_root]           = '/var/www/vayacondios'
+default[:vayacondios][:home_dir]              = '/var/www/vayacondios/current'
+default[:vayacondios][:conf_dir]              = '/var/www/vayacondios/shared/config'
+default[:vayacondios][:log_dir]               = '/var/www/vayacondios/shared/log'
+default[:vayacondios][:tmp_dir]               = '/var/www/vayacondios/shared/tmp'
+default[:vayacondios][:pid_dir]               = '/var/www' # it's www-data
 
-default[:vayacondios][:server][:port]        = 9000
-default[:vayacondios][:server][:auth_port]   = 9001
-default[:vayacondios][:server][:num_daemons] = 1
-default[:vayacondios][:server][:verbose]     = false
-default[:vayacondios][:server][:environment] = 'production'
+default[:vayacondios][:server][:port]         = 9000
+default[:vayacondios][:server][:auth_port]    = 9001
+default[:vayacondios][:server][:num_daemons]  = 1
+default[:vayacondios][:server][:verbose]      = false
+default[:vayacondios][:server][:environment]  = 'production'
+
+default[:vayacondios][:client][:conf_dir]     = '/etc/vayacondios'
 
 default[:vayacondios][:mongodb][:database]    = nil # set in role
 default[:vayacondios][:mongodb][:connections] = 20
 
-default[:vayacondios][:cleaner][:clean]   = true
-default[:vayacondios][:cleaner][:max_age] = "1h"
+default[:vayacondios][:cleaner][:clean]       = true
+default[:vayacondios][:cleaner][:max_age]     = "1h"
--- cookbooks/vayacondios/recipes/client.rb
+++ cookbooks/vayacondios/recipes/client.rb
@@ -1,27 +1,20 @@
-include_recipe("vayacondios::default")
+include_recipe 'vayacondios::default'
 
-standard_dirs "vayacondios" do
-  # do *not* include home_dir as it is actually a symlink that will
-  # be created by the install_from_git recipe below
-  #
-  # do *not* include log_dir as it is not required by the client, only
-  # the server
-  directories :deploy_root, :conf_dir
+standard_dirs 'vayacondios.client' do
+  directories :conf_dir
 end
-
-include_recipe("vayacondios::install_from_git")
 
 vayacondios = discover(:vayacondios, :server)
 
+Chef::Log.warn "This is the vayacondios you have discovered: #{vayacondios.private_ip}"
+
 Chef::Log.warn("No Vayacondios organization is set for this node (node[:vayacondios][:organization]).") unless node[:vayacondios][:organization]
 
-template File.join(node[:vayacondios][:conf_dir], "vayacondios.yml") do
-  source    "vayacondios.yml.erb"
-  mode      "0644"
-  variables({
-    :vayacondios  => {
-       :host => vayacondios && vayacondios.private_ip,
-       :port => vayacondios && vayacondios.ports[:nginx][:port],
-    },
-  })
+template File.join(node[:vayacondios][:client][:conf_dir], 'vayacondios.yml') do
+  source    'vayacondios.yml.erb'
+  mode      '0644'
+  variables(
+    host: vayacondios && vayacondios.private_ip,
+    port: vayacondios && vayacondios.ports[:nginx][:port],
+  )
 end
