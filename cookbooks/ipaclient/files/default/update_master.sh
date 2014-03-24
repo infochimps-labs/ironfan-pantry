5d91597e57679d1d446d085555c264e90e47a7c1
Erik Mackdanz <erik@infochimps.com>
2014-03-24 16:21:01 -0500

    Fix: remove shadowing cookbook.
    The desired ipaclient cookbook is in ironfan-enterprise
    
    Change-Id: Ib1949090566cb1b2d38f2df4617a7b62b1127d67

--- cookbooks/ipaclient/files/default/update_master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-hostsfile="/etc/hosts"
-# Bail out if the file is already updated
-grep $1 $hostsfile && exit 0
-MASTERIP=`dig +short $1`
-echo "#Updated at `date`" >> $hostsfile
-echo $MASTERIP $1 >> $hostsfile
