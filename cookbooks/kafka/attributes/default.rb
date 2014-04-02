7a39d8d549095ffe4282111c4e2a357abe140ca6
Josh Bronson <josh.bronson@gmail.com>
2014-04-01 21:06:35 -0500

    feature: pin to release_6
    Change-Id: Ifa3997c4a1afbc81fa22062daa0815f5ef5f76e2

--- cookbooks/kafka/attributes/default.rb
+++ cookbooks/kafka/attributes/default.rb
@@ -52,7 +52,7 @@ default[:tuning][:ulimit]['kafka']                  = { :nofile => { :both => 32
 #
 default[:kafka][:contrib][:deploy][:root]           = '/usr/local/share/kafka-contrib'
 default[:kafka][:contrib][:deploy][:repo]           = 'git@github.com:infochimps/kafka-contrib.git'
-default[:kafka][:contrib][:deploy][:branch]         = 'master'
+default[:kafka][:contrib][:deploy][:branch]         = 'release_6'
 default[:kafka][:contrib][:log_dir]                 = '/var/log/kafka-contrib'
 default[:kafka][:contrib][:auth_port]               = 80
 default[:kafka][:contrib][:app_port]                = 8080
