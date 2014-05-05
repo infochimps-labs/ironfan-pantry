311382bf38eb5450bd7b00baed23151977d01e1b
Erik Mackdanz <erik@infochimps.com>
2014-05-05 11:26:48 -0500

    srp_repo installs yum repo on rhel systems
    Change-Id: If4f92ee1599f578c8362ae1c3aa0a518192aeec0

--- cookbooks/cloud_utils/recipes/srp_repo.rb
+++ cookbooks/cloud_utils/recipes/srp_repo.rb
@@ -1,9 +1,18 @@
 
 environment=node[:cloud_utils][:srp][:environment]
 
-apt_repository 'srp-debian' do
-  uri "https://s3.amazonaws.com/srp-debian.chimpy.us/#{environment}/"
-  distribution '/'
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
+if platform_family?('debian')
+    apt_repository 'srp-debian' do
+      uri "https://s3.amazonaws.com/srp-debian.chimpy.us/#{environment}/"
+      distribution '/'
+      action :add
+      notifies :run, "execute[apt-get update]", :immediately
+    end
+elsif platform_family?('rhel')
+      yum_repository "srp-rhel" do
+        name "infochimps-#{environment}"
+        url "https://s3.amazonaws.com/srp-rhel.chimpy.us/#{environment}/"
+        # key "RPM-GPG-KEY-chimps"
+        action :add
+      end
 end
