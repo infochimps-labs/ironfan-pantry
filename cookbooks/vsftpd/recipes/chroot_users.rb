ad9fde33ae2c87fe331889042535a8780e43af30
Josh Bronson <josh.bronson@gmail.com>
2014-07-28 18:54:29 -0500

    fix: vsftpd conf file again, missed a spot
    Change-Id: Ib5f30bc10667465d70fb06d04bec7313c8a8d33f

--- cookbooks/vsftpd/recipes/chroot_users.rb
+++ cookbooks/vsftpd/recipes/chroot_users.rb
@@ -1,4 +1,4 @@
-template File.join(node[:vsftpd][:conf_dir], 'chroot_list.conf') do
+template node[:vsftpd][:conf_dir] do
   source "chroot_list.conf.erb"
   mode   '0644'
   notifies :restart, "service[vsftpd]", :delayed
